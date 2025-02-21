0b9f2fe141159e5fc54b0f8d1fc1a1084c98bc0b
Chinmay <chinmay.patil3@walchandsangli.ac.in>
2025-02-21 22:51:57 +0530

    Whole new update to Comment section (supports comments for any type of content not only Video but also Tweets)

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(cors({
     origin: ["http://localhost:5173", "https://youtube-frontend-two-rust.vercel.app"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 }));
 
 
