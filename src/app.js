f27e108f21339b9c3e47b2067bb44655108e2906
Chinmay <72600485+ChinmayOnGithub@users.noreply.github.com>
2025-06-03 22:30:19 +0530

    Fixed depricated error (Typo in 'extended')

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(cors({
 // common middlewares
 // app.use(express.json({ limit: "16kb" }));
 app.use(express.json());
-app.use(express.urlencoded({ extende: true, limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser())
 app.use(express.static("public"));
 
